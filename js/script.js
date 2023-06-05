cd5da921938c5a54a7d1facbd43f8eb2d7c4091d
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2023-06-05 21:41:46 +0700

    add scripts to its own file and do some css. There is a part that is not working properly yet. Pls finish it next time.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var nama = prompt("Masukkan nama kamu");
+    document.getElementById("nama").innerText = nama;
+
+
+function validateForm() {
+    var namaDepan = document.forms["message-form"]["nama-depan"].value;
+    var namaBelakang = document.forms["message-form"]["nama-belakang"].value;
+    var tanggalLahir = document.forms["message-form"]["tanggal"].value;
+    var genderKamu = document.forms["message-form"]["gender"].value;
+    var pesanKamu = document.forms["message-form"]["pesan"].value;
+
+    if (namaDepan == "" || namaBelakang == "" || tanggalLahir == "" || genderKamu == "" || pesanKamu == "") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    };
+
+    document.getElementById('sender-nama-depan').innerText = namaDepan;
+    document.getElementById('sender-nama-belakang').innerText = namaBelakang;
+    document.getElementById('sender-nama-tanggal-lahir').innerText = tanggalLahir;
+    document.getElementById('sender-gender').innerText = genderKamu;
+    document.getElementById('sender-pesan').innerText = pesanKamu;
+    return false;
+}
