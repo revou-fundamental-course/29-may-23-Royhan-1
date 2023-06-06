740cebdf8221e09f2cc9bb1a1044a45d8235acb8
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2023-06-06 16:54:15 +0700

    pindah script ke file dan permak sedikit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var nama = prompt("Masukkan nama kamu");
+document.getElementById("nama").innerText = nama;
+
 var form = document.getElementById("messageform");
 function handleForm(event) {
     event.preventDefault();
