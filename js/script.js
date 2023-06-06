e547e476e2edf5f87a8d0a48fb7b291e155dbf98
Royhan-1 <129375214+Royhan-1@users.noreply.github.com>
2023-06-06 16:49:43 +0700

    membenarkan fungsi js di html & permak profile page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
-var nama = prompt("Masukkan nama kamu");
-    document.getElementById("nama").innerText = nama;
-
-// Fix fungsi dibawah agar tidak reload saat submit
-function validateForm() {
-    var namaDepan = document.forms["message-form"]["nama-depan"].value;
-    var namaBelakang = document.forms["message-form"]["nama-belakang"].value;
-    var tanggalLahir = document.forms["message-form"]["tanggal"].value;
-    var genderKamu = document.forms["message-form"]["gender"].value;
-    var pesanKamu = document.forms["message-form"]["pesan"].value;
-
-    if (namaDepan == "" || namaBelakang == "" || tanggalLahir == "" || genderKamu == "" || pesanKamu == "") {
-        alert("Tidak boleh ada yang kosong");
-        return false;
-    };
-
-    document.getElementById('sender-nama-depan').innerText = namaDepan;
-    document.getElementById('sender-nama-belakang').innerText = namaBelakang;
-    document.getElementById('sender-nama-tanggal-lahir').innerText = tanggalLahir;
-    document.getElementById('sender-gender').innerText = genderKamu;
-    document.getElementById('sender-pesan').innerText = pesanKamu;
-    return false;
-}
+var form = document.getElementById("messageform");
+function handleForm(event) {
+    event.preventDefault();
+};
+form.addEventListener("submit", handleForm);
